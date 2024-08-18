a2eac2c18b29ac6532c8110b61da7b5d71257a50
VictorPersonal <tabareztor2005@gmail.com>
2024-08-18 17:37:44 -0500

    Agregue una rama y un cambio dentro de ella

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,4 +1,9 @@
-base=int(input("Ingrese el numero que se tomara como base: "))
-exponete=int(input("Ingrese el numero que se tomara como exponente: "))
-resultado=pow(base,exponete)
+
+import sys
+b=int(input("Ingrese el número que se tomará como base: "))
+e=int(input("Ingrese el número que se tomará como exponente: "))
+resultado=pow(b,e)
+print(sys.getsizeof(b))
+print(sys.getsizeof(e))
+print(sys.getsizeof(resultado))
 print(resultado)
