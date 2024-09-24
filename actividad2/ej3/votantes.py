ff919a8255f8874e3e94f46ce23211114af88b0d
VictorPersonal <tabareztor2005@gmail.com>
2024-09-24 09:17:46 -0500

    Borré el main; por lo que todo funciona en el archivo de votante.py

--- actividad2/ej3/votantes.py
+++ actividad2/ej3/votantes.py
@@ -6,26 +6,26 @@ class Votante():
         self.nombre=nombre
         self.edad=edad
         self.sexo=sexo
-        self.listaVotantes=[]
-
-    def ingresarAlVotante(self):
+    @staticmethod
+    def ingresarAlVotante():
+        lista=[]
         for votante in range(4):
             print(f"Votante {votante+1}")
             nombreVotante=input("Ingrese el nombre del votante: ")
             EdadVotante=int(input("Ingrese la edad del votante: "))
             sexo=input("Ingrese el sexo: (Preferiblemente F --> Femenino; M --> Masculino; O --> Otro): ")
-            if sexo in ["F","M","O"]:
-                return sexo
-            print("No se admite el sexo")
             votanteGuardado=Votante(nombreVotante,EdadVotante,sexo)
-            self.listaVotantes.append(votanteGuardado)
-            return self.listaVotantes
+            lista.append(votanteGuardado)
 
-    def selectVotantesPorEdad(self):
+        return lista
+            
+            
+    @staticmethod
+    def selectVotantesPorEdad(lista):
         mayores_a_18_menores_a_20=0
         mayores_20_menores_30=0
         mayores_30=0
-        for votante in self.listaVotantes:
+        for votante in lista:
             if 18<= votante.edad <= 20:
                 mayores_a_18_menores_a_20+=1
             elif 20 < votante.edad <= 30:
@@ -34,11 +34,13 @@ class Votante():
             else:
                 mayores_30+=1
         return mayores_a_18_menores_a_20, mayores_20_menores_30, mayores_30
-    
 
-    def mostrarResult(self):
-        rango_18_20, rango_21_30, rango_30= self.selectVotantesPorEdad(self.listaVotantes)
-        print(f"Resultados\na. Votantes entre 18 y 20 años: {rango_18_20}\nb. Votantes entre 21 y 30 años: {rango_21_30}\nc. Votantes mayores de 30 años: {rango_30}")
+
+print("Cargando lista....")
+lista_votantes=Votante.ingresarAlVotante()
+
+rango_18_20, rango_21_30, rango_30= Votante.selectVotantesPorEdad(lista_votantes)
+print(f"Resultados\na. Votantes entre 18 y 20 años: {rango_18_20}\nb. Votantes entre 21 y 30 años: {rango_21_30}\nc. Votantes mayores de 30 años: {rango_30}")
 
 
 
